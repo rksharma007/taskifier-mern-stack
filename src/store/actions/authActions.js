a5657f2f4015109eca8c6226d4de756d68d7f061
Ravi Kant Sharma <77050199+rksharma007@users.noreply.github.com>
2022-06-13 15:22:48 +0530

    Create authActions.js

---
+++ src/store/actions/authActions.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { url } from "../../api";
+
+export const signUp = (user) => {
+    return (dispatch) => {
+        axios
+        .post(`${ url}/signup`, user)
+        .then(token => {
+            localStorage.setItem("token", token.data)
+            dispatch({
+                type: "SIGN_UP",
+                token:  token.data
+            })
+        })
+        .catch( error =>{
+            console.log(error.response)
+            toast.error(error.response?.data ,{
+                position: toast.POSITION.BOTTOM_RIGHT
+            })
+        })
+    }
+}
+
+export const loadUser = () => {
+    return (dispatch, getState) => {
+        const token = getState().auth.token
+        if(token){
+            dispatch({
+                type: "USER_LOADED",
+                token
+            })
+        } else return null
+    }
+}
+
+export const signIn = (creds) => {
+    return (dispatch) => {
+        axios
+        .post(`${ url}/signin`, creds)
+        .then(token => {
+            localStorage.setItem("token", token.data)
+            dispatch({
+                type: "SIGN_IN",
+                token:  token.data
+            })
+        })
+        .catch( error =>{
+            console.log(error.response)
+            toast.error(error.response?.data ,{
+                position: toast.POSITION.BOTTOM_RIGHT
+            })
+        })
+    }
+}
+
+export const signOut = () => {
+    return (dispatch) => {
+        dispatch({
+            type: "SIGN_OUT"
+        })
+    }
+}
